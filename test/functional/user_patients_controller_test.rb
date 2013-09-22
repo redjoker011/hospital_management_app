556bc6694d8af4cb1ddaf2756867ae34c859e429
Prashant Jadhav <pjadhav.god@gmail.com>
2013-09-22 17:13:22 +1000

    modification

--- test/functional/user_patients_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UserPatientsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-end
--- test/unit/user_patient_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserPatientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
